a9c4dae8a37d5aab7205b99facfe861c35b2df53
dariusdav <darius.davidonis@gmail.com>
2022-03-02 13:26:25 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPage } from './pages/landing/landing.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPage,
+    TrainerPage,
+    PokemonCataloguePage
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
