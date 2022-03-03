b03564d57c178d41b81fb5679a10799ba9c18e32
dariusdav <darius.davidonis@gmail.com>
2022-03-03 15:01:28 +0100

    feat: Auth created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
 import { LandingPage } from "./pages/landing/landing.page";
 import { PokemonCataloguePage } from "./pages/pokemon-catalogue/pokemon-catalogue.page";
 import { TrainerPage } from "./pages/trainer/trainer.page";
@@ -12,15 +13,19 @@ const routes: Routes =[
     },
     {
         path: "login",
-        component: LandingPage
+        component: LandingPage,
     },
     {
         path: "catalogue",
-        component: PokemonCataloguePage
+        component: PokemonCataloguePage,
+        canActivate: [AuthGuard]
+
     },
     {
         path: "trainer",
-        component: TrainerPage
+        component: TrainerPage,
+        canActivate: [AuthGuard]
+
     }
 ]
 
