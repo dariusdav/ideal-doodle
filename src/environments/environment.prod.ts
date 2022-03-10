20c611f5c8f558a82dca3a872091f4eed09f8597
dariusdav <darius.davidonis@gmail.com>
2022-03-10 23:23:45 +0100

    feat: "all gen 1 pokeys are catchable"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUsers: "https://darius-api.herokuapp.com/trainers",
-  apiPokemon: "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",
+  apiPokemon: "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0",
   apiKey: "X,F94aSWJ7n}G#P%@48Qg8k[nA}{rAF?%7VCeH3Rf52}hz/9[(M%]z46.!Bqdp.b",
   apiPokemonPicture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
 };
